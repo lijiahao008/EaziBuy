f4e10e705cf6aac079358249fbff9fdf9675f106
Jiahao Li <lijiahao008@gmail.com>
2017-05-12 19:54:47 -0400

    set up react

---
+++ react/components/app.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const App = ({ children }) => (
+  <div>
+    <header>
+    </header>
+    <main>
+      {children}
+    </main>
+  </div>
+);
+
+export default App;
